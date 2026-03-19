4597c09714e865f8736642eaeaadb60fe85e1997
Prakshal Jain <prakshaljain927@gmail.com>
2026-03-19 23:07:03 +0530

    Initial commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,7 +8,7 @@ import {
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import SectionHeading from "@/components/SectionHeading";
-import heroImg from "@/assets/hero-illustration.png";
+import heroImg from "@/assets/image copy.png";
 
 const fadeUp = {
   hidden: { opacity: 0, y: 24 },
